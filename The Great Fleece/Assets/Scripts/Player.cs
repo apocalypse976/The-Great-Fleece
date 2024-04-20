5f17933154e5c33ad0a680c9f934427d08cff0f4
apocalypse976 <aarunichatterjee@gmail.com>
2024-04-20 09:46:12 +0530

    Player Movements

--- The Great Fleece/Assets/Scripts/Player.cs
+++ The Great Fleece/Assets/Scripts/Player.cs
@@ -5,20 +5,18 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private GameObject _dareen;
     private NavMeshAgent _agent;
     private Animator _anim;
-    private float _distance;
-    private Vector3 _target;
     private void Start()
     {
-        _agent = GetComponentInChildren<NavMeshAgent>();
-        _anim = GetComponentInChildren<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Movements();
+        Animations();
     }
 
     void Movements()
@@ -30,23 +28,21 @@ public class Player : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-               _agent.SetDestination(hit.point);
-                _anim.SetBool("Walk", true);
-                _target = hit.point;
-
+                _agent.SetDestination(hit.point);
             }
 
         }
-        //_distance = Vector3.Distance(transform.position, _target);
-        //Debug.Log(_distance);
-        //if (_distance < 1.0f)
-        //{
-        //    _anim.SetBool("Walk", false);
-        //    Debug.Log("Distance Reached");
-        //}
-        if (_dareen.transform.position == _target)
+    
+    }
+    void Animations()
+    {
+        if (_agent.velocity.magnitude > 0)
         {
-            Debug.Log("Distance Reached");
+            _anim.SetBool("Walk", true);
+        }
+        else if( _agent.velocity.magnitude<= 0)
+        {
+            _anim.SetBool("Walk", false);
         }
     }
 }
